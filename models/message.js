2cc9317bc72b80811a58cf497d12000fd8431ded
kblack-git <keithblack@gmail.com>
2023-05-04 13:53:58 -0400

    setup modela & ran migrations

---
+++ models/message.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class message extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Order.belongsTo(models.User, {
+        foreignKey: 'user_id',
+      });
+    }
+  }
+  message.init({
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    date: DataTypes.INTEGER,
+    note: DataTypes.STRING,
+    user_id: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'message',
+  });
+  return message;
+};
---
+++ models/user.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class user extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      User.hasMany(models.Message, {
+        foreignKey: 'user_id',
+      });
+    }
+  }
+  user.init({
+    firstname: DataTypes.STRING,
+    lastname: DataTypes.STRING,
+    username: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'user',
+  });
+  return user;
+};
