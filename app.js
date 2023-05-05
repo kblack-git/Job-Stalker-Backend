0ed588b6474da0c9ddb0061d048badd79fe9b9a8
kblack-git <keithblack@gmail.com>
2023-05-04 21:27:46 -0400

    add messages route and post request

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var messagesRouter = require('./routes/messages');
 var app = express();
 
 app.use(logger('dev'));
---
+++ routes/messages.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+// Add your resource-specific routes here
+const { Message} = require('../models');
+
+// Create a new message
+router.post('/', async (req, res) => {
+  try {
+    const message = await Message.create(req.body);
+    res.status(201).json(Message);
+  } catch (error) {
+    res.status(500).json({ message: 'Error creating message', error });
+  }
+});
