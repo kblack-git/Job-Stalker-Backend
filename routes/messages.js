f360aaeda61f1e06c8ba4f7f4838994280d160a6
kblack-git <keithblack@gmail.com>
2023-05-04 22:03:02 -0400

    fixed syntax error

--- routes/messages.js
+++ routes/messages.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 // Add your resource-specific routes here
-const { Message} = require('../models');
+const { Message } = require('../models');
 
 // Create a new message
 router.post('/', async (req, res) => {
   try {
     const message = await Message.create(req.body);
-    res.status(201).json(Message);
+    res.status(201).json(message);
   } catch (error) {
     res.status(500).json({ message: 'Error creating message', error });
   }
