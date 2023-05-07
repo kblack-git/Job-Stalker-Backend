04368bd7ff64836ab0aa6c6aa0807208af9c64d4
kblack-git <keithblack@gmail.com>
2023-05-06 20:26:54 -0400

    added users route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const { user } = require('../models');
+
+// Create a new message
+router.get('/', async(req,res)=>{
+  try {
+    const theUser = await user.findAll(req.body);
+    res.status(201).json(theUser);
+  } catch (error) {
+    res.status(500).json({ message: 'Error getting user', error });
+  }
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const theUser = await user.create(req.body);
+    res.status(201).json(theUser);
+  } catch (error) {
+    res.status(500).json({ message: 'Error creating user', error });
+  }
 });
+
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
 
 module.exports = router;
