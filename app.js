2f521c881323b6c2313c693296e19871f08d35fa
kblack-git <keithblack@gmail.com>
2023-05-08 10:05:52 -0400

    added auth route with JWT

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const passport = require('passport');
+require('./config/passport')(passport);
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var messagesRouter = require('./routes/messages');
+var authRouter = require('./routes/auth');
 var app = express();
 
 app.use(logger('dev'));
@@ -13,9 +17,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/messages', messagesRouter);
+app.use('/auth', authRouter);
 
 module.exports = app;
---
+++ config/passport.js
@@ -0,0 +1,26 @@
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const User = require('../models').User;
+
+const opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: 'your_jwt_secret', // using an environment variable for this value is a good idea
+};
+
+module.exports = (passport) => {
+  passport.use(
+    new JwtStrategy(opts, async (jwt_payload, done) => {
+      try {
+        const user = await User.findByPk(jwt_payload.id);
+        if (user) {
+          return done(null, user);
+        }
+
+        return done(null, false);
+      } catch (error) {
+        return done(error, false);
+      }
+    })
+  );
+};
+
